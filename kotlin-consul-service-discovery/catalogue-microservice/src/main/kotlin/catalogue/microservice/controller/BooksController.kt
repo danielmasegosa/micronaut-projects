2e00b475b560ddc6568d320b0e3054f77757c221
Daniel Masegosa <danielmasegosa@hotmail.com>
2019-03-30 13:47:08 +0100

    Updated catalogue-microservice

--- kotlin-consul-service-discovery/catalogue-microservice/src/main/kotlin/catalogue/microservice/controller/BooksController.kt
+++ kotlin-consul-service-discovery/catalogue-microservice/src/main/kotlin/catalogue/microservice/controller/BooksController.kt
@@ -2,11 +2,13 @@ package catalogue.microservice.controller
 
 import catalogue.microservice.model.Book
 import catalogue.microservice.service.BooksService
+import com.sun.xml.internal.ws.spi.db.BindingContextFactory.LOGGER
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
 import io.micronaut.http.annotation.Post
 import io.micronaut.runtime.server.EmbeddedServer
 import io.micronaut.validation.Validated
+import org.slf4j.LoggerFactory
 import javax.validation.constraints.NotEmpty
 
 @Validated
@@ -14,15 +16,17 @@ import javax.validation.constraints.NotEmpty
 class BooksController(val booksService: BooksService,
                       val embeddedServer: EmbeddedServer) {
 
+    private val logger = LoggerFactory.getLogger(this.javaClass)
+
     @Get
     internal fun getAllBooks() : List<Book> {
-        print(embeddedServer.port)
+        logger.info("Running on port ${embeddedServer.port}")
         return booksService.findAllBooks()
     }
 
     @Post
     internal fun insertBook(@NotEmpty book: Book): Book {
-        print(embeddedServer.port)
+        logger.info("Running on port ${embeddedServer.port}")
         return booksService.insertBook(book)
     }
 }
